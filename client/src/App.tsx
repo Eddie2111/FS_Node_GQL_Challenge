cdf74c3412972522af6ade43cf6daaef543795db
Eddie2111 <asm.tareq.mahmood@g.bracu.ac.bd>
2024-07-10 00:29:17 +0600

    refactor: home page tidy up with connection state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { useQuery, gql } from "@apollo/client";
+
 function App() {
   const GET_Hello = gql`
     query HelloWorld {
@@ -6,13 +7,34 @@ function App() {
     }
   `;
   const { loading, error, data } = useQuery(GET_Hello);
-  console.log(loading, error, data);
-  return (
-    <div className="mx-auto container">
-      <h1 className="font-bold text-[8rem] text-center md:text-[12rem]">TeeBay</h1>
-      <p className="text-lg">Here, you can add, rent and buy products</p>
-    </div>
-  );
+  if(loading) {
+    return (
+      <div className="mx-auto container">
+        <h1 className="font-bold text-[8rem] text-center md:text-[12rem]">
+          TeeBay
+        </h1>
+        <p className="text-lg">Loading and connecting to graphql api</p>
+      </div>
+    );
+  }
+    if(error) {
+    return (
+      <div className="mx-auto container">
+        <h1 className="font-bold text-[8rem] text-center md:text-[12rem]">
+          TeeBay
+        </h1>
+        <p className="text-lg">Failed to connect to backend graphql</p>
+      </div>
+    );
+  }
+  if (data) {
+    return (
+      <div className="mx-auto container">
+        <h1 className="font-bold text-[8rem] text-center md:text-[12rem]">TeeBay</h1>
+        <p className="text-lg">Here, you can add, rent and buy products</p>
+      </div>
+    );
+  }
 }
 
 export default App;
